23d38e15392b114c11e36b7806c45a013d6f2a35
Jed Morris <jemorris110@gmail.com>
2022-01-20 08:30:07 -0800

    created movieObj

--- js/site.js
+++ js/site.js
@@ -4,10 +4,17 @@ function addLink() {
 	let movieName = document.getElementById("movieName").value;
 	let movieUrl = document.getElementById("movieUrl").value;
 
+	let movieObj = {};
 
+	movieObj["name"] = movieName;
+	movieObj["url"] = movieUrl;
+
+	displayLink(movieObj);
+	
 }
 
 // writes the link to the page
-function displayLink() {
+function displayLink(movieObj) {
 
+	let ol = document.getElementById("movieList");
 }
