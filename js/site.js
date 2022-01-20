93470d9a70198c87fe30ed75596ada4dd1d4a9b7
Jed Morris <jemorris110@gmail.com>
2022-01-20 08:23:18 -0800

    created addLink and displayLink in site.js

---
+++ js/site.js
@@ -0,0 +1,13 @@
+// add a link
+function addLink() {
+	
+	let movieName = document.getElementById("movieName").value;
+	let movieUrl = document.getElementById("movieUrl").value;
+
+
+}
+
+// writes the link to the page
+function displayLink() {
+
+}
